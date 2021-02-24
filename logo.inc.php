b0ab118d19cb1078031f812a8a27eff8b7cb425f
Diana3002 <79607195+Diana3002@users.noreply.github.com>
2021-02-25 00:26:50 +0300

    Add files via upload

---
+++ logo.inc.php
@@ -0,0 +1,3 @@
+<div class="logo"> 
+                <img src="https://www.rea.ru/ru/org/cathedries/inyaz2kaf/PublishingImages/Pages/studymaterials/galochka.png" alt="php">
+                </div>        
